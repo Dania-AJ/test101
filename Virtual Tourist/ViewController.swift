0b281756351785378ba84ce81f4a14e286bbc3f2
Dania A <dania@danias-macbook-pro.local>
2018-12-25 16:40:02 +0300

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Dania A on 25/12/2018.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
